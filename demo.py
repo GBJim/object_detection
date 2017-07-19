bbc2485107c869be2c5755c9de9bbec93c9c87cd
chunhao chang <ccha97u@gmail.com>
2017-07-19 14:20:54 +0800

    demo.py supoort model selecting in command line

--- demo.py
+++ demo.py
@@ -10,6 +10,7 @@ from io import StringIO
 from PIL import Image
 import time
 import label_map_util
+import argparse
 
 MODELS = ['ssd_mobilenet_v1_coco_11_06_2017', 'ssd_inception_v2_coco_11_06_2017',\
                   'rfcn_resnet101_coco_11_06_2017', 'faster_rcnn_resnet101_coco_11_06_2017',\
@@ -23,6 +24,17 @@ PATH_TO_LABELS = "/root/object_detection/data/mscoco_label_map.pbtxt"
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
+
+#
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tensor Flow Detection Models')
+    parser.add_argument('--net', dest='net', help='Which model to specify',
+                        default=1, type=int, choices=[1,2,3,4,5])
+    args = parser.parse_args()
+    return args
+
+
+
 
 
 #Get the session from the specified model
@@ -100,10 +112,12 @@ def detect(sess, img_path, thresh=0.7):
                             img_height,thresh)
     
 if __name__ == "__main__":
-    TEST_IMAGE_PATHS = [os.path.join("/root/object_detection/test_images",\
-                                     'image{}.jpg'.format(i)) for i in range(1,3)]
+
+    args = parse_args()
+
+    TEST_IMAGE_PATHS = [os.path.join("/root/object_detection/test_images",\                                   'image{}.jpg'.format(i)) for i in range(1,3)]
     THRESHOLD = 0.7
-    model = MODELS[3]
+    model = MODELS[args.net]
     sess = load_model(model)
     for img_path in TEST_IMAGE_PATHS:
         result = detect(sess, img_path, thresh=THRESHOLD)
