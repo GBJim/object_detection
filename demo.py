d5af285a060cffef61a56281787b6c5f3c07e265
GBJim <ccha97u@gmail.com>
2018-01-15 16:17:54 +0800

    modify demo.py and demo.ipynb

--- demo.py
+++ demo.py
@@ -5,27 +5,27 @@ import time
 import label_map_util
 import argparse
 import cv2
-MODELS = ['ssd_mobilenet_v1_coco_11_06_2017', 'ssd_inception_v2_coco_11_06_2017',\
-                  'rfcn_resnet101_coco_11_06_2017', 'faster_rcnn_resnet101_coco_11_06_2017',\
-                   'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017']
+import glob
+
+MODELS = ['faster_rcnn_inception_v2_coco_2017_11_08']
 
 
 
 
 NUM_CLASSES = 90
-PATH_TO_LABELS = "/root/object_detection/data/mscoco_label_map.pbtxt"
+PATH_TO_LABELS = "data\mscoco_label_map.pbtxt"
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-#
+'''
 def parse_args():
     parser = argparse.ArgumentParser(description='Tensor Flow Detection Models')
     parser.add_argument('--net', dest='net', help='Which model to specify:  1.SSD-Mobilenet, 2.SSD-Inception, 3.RFCN-ResNet, 4.Faster-RCNN-Resnet, 5.Faster-RCNN-Inception-Resnet',
                         default=1, type=int, choices=[1,2,3,4,5])
     args = parser.parse_args()
     return args
-
+'''
 
 
 
@@ -37,7 +37,7 @@ def load_model(model, dynamic_memory=True):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth=dynamic_memory
     sess = tf.Session(config=config)    
-    PATH_TO_CKPT = os.path.join("/root/object_detection", model , 'frozen_inference_graph.pb')
+    PATH_TO_CKPT = os.path.join(model , 'frozen_inference_graph.pb')
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
@@ -111,11 +111,11 @@ def detect(sess, img_path, thresh=0.7):
     
 if __name__ == "__main__":
 
-    args = parse_args()
+  
 
-    TEST_IMAGE_PATHS = [os.path.join("/root/object_detection/test_images", 'image{}.jpg'.format(i)) for i in range(1,3)]
+    TEST_IMAGE_PATHS = glob.glob("test_images/*.jpg")
     THRESHOLD = 0.7
-    model = MODELS[args.net-1]
+    model = MODELS[0]
     sess = load_model(model) 
    
     for img_path in TEST_IMAGE_PATHS:
